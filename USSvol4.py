2b9190b38597aa81cfcd6d84ed386ab2b45d8d49
unknown <mariamurumaaa@gmail.com>
2018-11-27 13:56:54 +0200

    Lisasin lõpupildid

--- USSvol4.py
+++ USSvol4.py
@@ -3,6 +3,78 @@ from math import sqrt
 from random import randrange
 import time
 
+def üksikmängija(a):
+    while tõeväärtus == True:
+        pygame.time.delay(20)
+        
+	#Kui kasutaja paneb aknast kinni
+	if event.type == pygame.QUIT:
+            tõeväärtus = False
+			
+	#Uss sööb toitu
+	dist = sqrt((x+(laius_uss / 2)-x1)*(x+(laius_uss / 2)-x1) + (y+(kõrgus_uss / 2)-y1)*(y+(kõrgus_uss / 2)-y1))
+	if dist <= 15:
+		skoor += 1
+		if skoor > highscore:#Kui skoor suurem kui highscore ss highscore muutub scoreiks
+			highscore = skoor
+		x1 = randrange(border+raadius, laius-border-raadius, 25)
+		y1 = randrange(border+raadius, kõrgus-border-raadius, 25)
+		keha.append((x, y, laius_uss, kõrgus_uss))#Sisestame uued kordinaadid kehale
+	
+	#Ussi tagumisest otsast hakkavad kordinaadid ennemaks muutuma, ehk et viimane taguots muutub ruuduks, mis ta ees oli enne jne, kuni selleni mis enne pead on
+	for i in range(len(keha)-1, 0, -1):
+		keha[i] = keha[i-1]
+		pygame.draw.rect(aken, (255,204,204), (keha[i]))
+		
+	#Enne pead ruut liigub pea kohale, sest järgnevalt liigub pea edasi
+	if len(keha) > 0:
+		keha[0] = (x, y, laius_uss, kõrgus_uss)
+		pygame.draw.rect(aken, (255,204,204), (keha[0]))
+		
+	#Liikumine ja bordertest mitte välja liikumine  
+	keys = pygame.key.get_pressed()
+	if keys [pygame.K_LEFT]and x > border and x_muutus != 25:
+		x_muutus = -25
+		y_muutus = 0
+	elif keys [pygame.K_RIGHT]and x < (laius - laius_uss - border) and x_muutus != -25:
+		x_muutus = 25
+		y_muutus = 0
+	elif keys [pygame.K_UP] and y > border and y_muutus != 25:
+		y_muutus = -25
+		x_muutus = 0
+	elif keys [pygame.K_DOWN] and y < (kõrgus - laius_uss - border) and y_muutus != -25:
+		y_muutus = 25
+		x_muutus = 0
+	x = x + x_muutus
+	y = y + y_muutus
+
+	
+	#Seina kokkupõrge või uued x ja y kordinaadid on juba keha kordinaadi. Mäng läbi
+	if x < border or x > (laius - laius_uss - border) or y < border or y > (laius - laius_uss - border) or (x, y, laius_uss, kõrgus_uss) in keha:
+		surm = 1
+		x_muutus = 0
+		y_muutus = 0
+		f = open("highscore.txt", "w")
+		f.write(str(highscore))
+
+	if surm == 1:
+		lõpp = pygame.display.set_mode((laius,kõrgus))
+		if highscore > alghighscore: #Kontrollime kas saavutati uus highscore ja väljastame vastava sõnumi
+			pygame.display.set_caption("Läbi! Saavutasid uue parima skoori! Uus parim skoor: " + str(highscore))
+		if highscore <= alghighscore:
+			pygame.display.set_caption("Läbi! Parim skoor jäi muutumata! Mängu skoor: " + str(skoor) + " Parim skoor: " + str(highscore))
+		
+		aken.blit(mäng_läbi, (39,40))
+		   
+	pygame.draw.rect(aken, (252,166,166), (x, y, laius_uss, kõrgus_uss))
+	if surm == 0:
+		pygame.draw.circle(aken, (248, 255, 1), (x1, y1), raadius)
+		pygame.display.set_caption("Ussimäng! Skoor: " + str(skoor) + " Parim skoor: " + str(highscore))
+	pygame.display.update()
+	aken.fill((153, 255, 51))
+	time.sleep (100.0 / 1000.0)
+
+	
 try:#Võtame failist highscore ja faili puudumisel oletame, et highscore on 0
     highscore = int(open("highscore.txt").read())
 except:
@@ -43,76 +115,15 @@ x1 = randrange(border+raadius, laius-border-raadius, 25)
 y1 = randrange(border+raadius, kõrgus-border-raadius, 25)
 skoor = 0
 
+mängijad = pygame.key.get_pressed()
+
 tõeväärtus = True
 while tõeväärtus:
-    pygame.time.delay(20)
-    for event in pygame.event.get():
-        
-        #Kui kasutaja paneb aknast kinni
-        if event.type == pygame.QUIT:
-            tõeväärtus = False
-            
-    #Uss sööb toitu
-    dist = sqrt((x+(laius_uss / 2)-x1)*(x+(laius_uss / 2)-x1) + (y+(kõrgus_uss / 2)-y1)*(y+(kõrgus_uss / 2)-y1))
-    if dist <= 15:
-        skoor += 1
-        if skoor > highscore:#Kui skoor suurem kui highscore ss highscore muutub scoreiks
-            highscore = skoor
-        x1 = randrange(border+raadius, laius-border-raadius, 25)
-        y1 = randrange(border+raadius, kõrgus-border-raadius, 25)
-        keha.append((x, y, laius_uss, kõrgus_uss))#Sisestame uued kordinaadid kehale
-    
-    #Ussi tagumisest otsast hakkavad kordinaadid ennemaks muutuma, ehk et viimane taguots muutub ruuduks, mis ta ees oli enne jne, kuni selleni mis enne pead on
-    for i in range(len(keha)-1, 0, -1):
-        keha[i] = keha[i-1]
-        pygame.draw.rect(aken, (255,204,204), (keha[i]))
-        
-    #Enne pead ruut liigub pea kohale, sest järgnevalt liigub pea edasi
-    if len(keha) > 0:
-        keha[0] = (x, y, laius_uss, kõrgus_uss)
-        pygame.draw.rect(aken, (255,204,204), (keha[0]))
-        
-    #Liikumine ja bordertest mitte välja liikumine  
-    keys = pygame.key.get_pressed()
-    if keys [pygame.K_LEFT]and x > border and x_muutus != 25:
-        x_muutus = -25
-        y_muutus = 0
-    elif keys [pygame.K_RIGHT]and x < (laius - laius_uss - border) and x_muutus != -25:
-        x_muutus = 25
-        y_muutus = 0
-    elif keys [pygame.K_UP] and y > border and y_muutus != 25:
-        y_muutus = -25
-        x_muutus = 0
-    elif keys [pygame.K_DOWN] and y < (kõrgus - laius_uss - border) and y_muutus != -25:
-        y_muutus = 25
-        x_muutus = 0
-    x = x + x_muutus
-    y = y + y_muutus
-
-    
-    #Seina kokkupõrge või uued x ja y kordinaadid on juba keha kordinaadi. Mäng läbi
-    if x < border or x > (laius - laius_uss - border) or y < border or y > (laius - laius_uss - border) or (x, y, laius_uss, kõrgus_uss) in keha:
-        surm = 1
-        x_muutus = 0
-        y_muutus = 0
-        f = open("highscore.txt", "w")
-        f.write(str(highscore))
-
-    if surm == 1:
-        lõpp = pygame.display.set_mode((laius,kõrgus))
-        if highscore > alghighscore: #Kontrollime kas saavutati uus highscore ja väljastame vastava sõnumi
-            pygame.display.set_caption("Läbi! Saavutasid uue parima skoori! Uus parim skoor: " + str(highscore))
-        if highscore <= alghighscore:
-            pygame.display.set_caption("Läbi! Parim skoor jäi muutumata! Mängu skoor: " + str(skoor) + " Parim skoor: " + str(highscore))
-        
-        aken.blit(mäng_läbi, (39,40))
-           
-    pygame.draw.rect(aken, (252,166,166), (x, y, laius_uss, kõrgus_uss))
-    if surm == 0:
-        pygame.draw.circle(aken, (248, 255, 1), (x1, y1), raadius)
-        pygame.display.set_caption("Ussimäng! Skoor: " + str(skoor) + " Parim skoor: " + str(highscore))
-    pygame.display.update()
     aken.fill((153, 255, 51))
-    time.sleep (100.0 / 1000.0)
+    if mängijad [pygame.K_1]:
+        üksikmängija(1)
+        
+
+
 
 pygame.quit()
